91a3a27eafd545779efa3ef9a5223224a31bb68a
Mar123ian <91825068+Mar123ian@users.noreply.github.com>
2024-08-23 19:18:29 +0300

    Create rage_quit.py

---
+++ Python/Python Fundamentals/Text Processing/rage_quit.py
@@ -0,0 +1,24 @@
+def add_word(text):
+    n=" "
+    t=""
+
+    for el in text:
+        if el.isnumeric():
+            if t[len(t)-1]!="|":
+                t+="|"
+            n+=el
+        else:
+            if n[len(n)-1]!="|":
+                n+="|"
+            t+=el
+    n=n.split("|")
+    t=t.split("|")
+    t.remove("")
+    n.remove(" ")
+    length=len(set(("".join(t)).upper()))
+    print(f"Unique symbols used: {length}")
+    for i in range(len(t)):
+	    print((t[i]*int(n[i])).upper(),end="")
+       
+text=input()
+add_word(text)
