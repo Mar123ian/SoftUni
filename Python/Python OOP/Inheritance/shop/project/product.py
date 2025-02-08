c8669b5d8ebe1cb7d679157224ee509713377a2a
Mar123ian <91825068+Mar123ian@users.noreply.github.com>
2025-02-08 20:46:04 +0200

    Add files via upload

---
+++ Python/Python OOP/Inheritance/shop/project/product.py
@@ -0,0 +1,16 @@
+class Product:
+    def __init__(self,name: str, quantity: int ):
+        self.quantity = quantity
+        self.name = name
+
+    def decrease(self, quantity: int):
+        new_quantity=self.quantity-quantity
+
+        if new_quantity>=0:
+            self.quantity=new_quantity
+
+    def increase(self, quantity: int):
+       self.quantity+=quantity
+
+    def __repr__(self):
+        return self.name
