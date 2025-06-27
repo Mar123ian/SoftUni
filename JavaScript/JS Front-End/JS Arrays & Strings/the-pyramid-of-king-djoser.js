605cab50d9e00313cbfebecea0dbb341a69dd511
марио марио <mario.mg.sz.08@gmail.com>
2025-06-27 22:45:37 +0300

    Added Excercises

---
+++ JavaScript/JS Front-End/JS Arrays & Strings/the-pyramid-of-king-djoser.js
@@ -0,0 +1,34 @@
+function thePyramidOfKingDjoser(size, step){
+    let stone=0;
+    let marble=0;
+    let lapisLazuli=0;
+    let floor=0;
+    for (i=size-2;i>0;i-=2){
+        floor++;
+        stone+=i**2*step;
+        if (floor%5!=0){
+            marble+=(i*4+4)*step;
+        }
+        else{
+            lapisLazuli+=(i*4+4)*step;
+        }
+        
+    }
+
+    console.log("Stone required: "+Math.ceil(stone))
+    console.log("Marble required: "+Math.ceil(marble))
+    console.log("Lapis Lazuli required: "+Math.ceil(lapisLazuli))
+
+    if (size%2==0){
+        console.log("Gold required: 4")
+    }
+
+    else{
+        console.log("Gold required: 1")
+    }
+
+    console.log("Final pyramid height: "+Math.floor(floor*step+1))
+
+}
+
+thePyramidOfKingDjoser(13,1);
