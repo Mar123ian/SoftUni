425cf55260075e46fc55f8f856273a9f5480cfe7
марио марио <mario.mg.sz.08@gmail.com>
2025-06-30 18:48:32 +0300

    Added Excercises

--- JavaScript/JS Front-End/Functions and Statements/points-validation.js
+++ JavaScript/JS Front-End/Functions and Statements/points-validation.js
@@ -3,19 +3,15 @@ function pointsValidation(points){
     let pointsToCompare=[[0,0],[points[1][0], points[1][1]]];
     let comparisons=0;
 
-    for (let group of pointsToCompare){
-        let pointToComparex=group[0];
-        let pointToComparey=group[1];
+    for (let [pointToComparex, pointToComparey] of pointsToCompare){
 
-        for (let group2 of points){
-            
+        for (let [pointx, pointy] of points){
+
             comparisons++;
             if (comparisons>3){               
                 break;
             }
 
-            let pointx=group2[0];
-            let pointy=group2[1];
             if (Number.isInteger(Math.sqrt((pointToComparex-pointx)**2+(pointToComparey-pointy)**2))){
                 console.log(`{${pointx}, ${pointy}} to {${pointToComparex}, ${pointToComparey}} is valid`);
             }
---
+++ JavaScript/JS Front-End/test.js
@@ -0,0 +1,10 @@
+function convertJSON(name, lastName, hairColor) {
+ let person = {
+ name,
+ lastName,
+ hairColor
+};
+ console.log(JSON.stringify);
+}
+
+convertJSON("kk", "ss", "s")
