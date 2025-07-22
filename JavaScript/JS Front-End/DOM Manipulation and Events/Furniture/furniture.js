f34f167a588ed8b3e6ef97e350d7d57f39c1daca
марио марио <mario.mg.sz.08@gmail.com>
2025-07-22 20:59:09 +0300

    Added Excercises

--- JavaScript/JS Front-End/DOM Manipulation and Events/Furniture/furniture.js
+++ JavaScript/JS Front-End/DOM Manipulation and Events/Furniture/furniture.js
@@ -3,100 +3,111 @@ document.addEventListener('DOMContentLoaded', solve);
 function solve() {
   let generate=document.querySelector('#input input[type="submit"]');
   generate.addEventListener('click', generateRow);
-  let input=document.querySelector('#input textarea')
-  let body=document.querySelector('table tbody')
-
+  let input=document.querySelector('#input textarea');
+  let body=document.querySelector('table tbody');
+  let row;
   let buyButton=document.querySelector('#shop input[type="submit"]');
   buyButton.addEventListener('click', buy);
-
+  
+  
+  function createTd(type, info=undefined){
+    
+    if (type=="text"){
+      td = document.createElement("td");
+      let p= document.createElement("p");
+      p.textContent=info;
+      td.appendChild(p);
+      row.appendChild(td);
+    }
+    
+    if (type=="img"){
+      let td = document.createElement("td");
+      let image= document.createElement("img");
+      image.src=info;
+      td.appendChild(image);
+      row.appendChild(td);
+    }
+    
+    if (type=="checkbox"){
+      td = document.createElement("td");
+      let input= document.createElement("input");
+      input.type="checkbox";
+      
+      td.appendChild(input);
+      row.appendChild(td);
+    }
+    
+    
+  }
+  
   function generateRow(e){
     e.preventDefault();
     let data=(JSON.parse(input.value));
     
-    
-    console.log(data);
     for (el of data){
-      console.log(el);
-      let row = document.createElement("tr");
+      
+      row = document.createElement("tr");
       let {decFactor, img, name, price}=el;
-      let td = document.createElement("td");
-      let image= document.createElement("img")
-      image.src=img;
-      td.appendChild(image)
-      row.appendChild(td)
-
-      td = document.createElement("td");
-      let p= document.createElement("p")
-      p.textContent=name;
-      td.appendChild(p)
-      row.appendChild(td)
       
-
-      td = document.createElement("td");
-      p= document.createElement("p")
-      p.textContent=price;
-      td.appendChild(p)
-      row.appendChild(td)
+      createTd("img", img);
+      createTd("text", name);
+      createTd("text", price);
+      createTd("text", decFactor);
+      createTd("checkbox");
       
-      td = document.createElement("td");
-      p= document.createElement("p")
-      p.textContent=decFactor;
-      td.appendChild(p)
-      row.appendChild(td)
-
-      td = document.createElement("td");
-      let input= document.createElement("input")  
-      input.type="checkbox"
+      body.appendChild(row);
       
-      td.appendChild(input)
-      row.appendChild(td)
-
-      body.appendChild(row)
-
     }
     
   }
+  
+  let tbody=document.querySelector('tbody');
+  tbody.addEventListener('change', addToCart);
+  
+  let cart=[];
+  let totalPrice=0;
+  let totalDecFactor=0;
 
-  function buy(e){
-    e.preventDefault();
-    let cart=[];
-    let total=0;
-    let adc=0;
-    let pr=0;
-    let els=Array.from(document.querySelectorAll("tr"));
-    els.shift()
-    for (el of els){
-      let checkbox=el.querySelector('input[type="checkbox"]')
-      console.log(el,checkbox);
+  function addToCart(e){
+    
+    let checkbox=e.target;
+    if (checkbox.tagName=="INPUT"){
+      let name=checkbox.parentNode.parentNode.querySelector("td:nth-child(2) p").textContent;
+      let price=Number(checkbox.parentNode.parentNode.querySelector("td:nth-child(3) p").textContent);
+      let decFactor=Number(checkbox.parentNode.parentNode.querySelector("td:nth-child(4) p").textContent);
+      
       if (checkbox.checked){
-        console.log(checkbox.parentNode.parentNode.querySelector("td:nth-child(2) p"));
-        let name=checkbox.parentNode.parentNode.querySelector("td:nth-child(2) p").textContent;
-        let price=Number(checkbox.parentNode.parentNode.querySelector("td:nth-child(3) p").textContent);
-        let dc=Number(checkbox.parentNode.parentNode.querySelector("td:nth-child(4) p").textContent);
-        adc+=dc
+        
         cart.push(name);
-        total+=price;
-        pr++;
-
-   
+        totalPrice+=price;
+        totalDecFactor+=decFactor;
       }
-
+      
+      else{
+        cart.splice(cart.indexOf(name), 1);
+        totalPrice-=price;
+        totalDecFactor-=decFactor;
+      }
     }
-    let output=document.querySelector("#shop textarea")
-    console.log(cart, total);
-    let bf=cart.join(", ")
-    let out=[]
-    out.push("Bought furniture: "+bf)
-    out.push(`Total price: ${total}`)
-    if (adc!=0){
-      out.push(`Average decoration factor: ${adc/pr}`)
+    
+  }
+  
+  
+  function buy(e){
+    e.preventDefault(); 
+    let outputArea=document.querySelector("#shop textarea");
+    let cartToString=cart.join(", ");
+    let output=`Bought furniture: ${cartToString}\nTotal price: ${totalPrice}\n`;
+    
+    if (totalDecFactor!=0){
+      output+=`Average decoration factor: ${totalDecFactor/cart.length}`;
     }
     else{
-      out.push('Average decoration factor: 0')
+      output+='Average decoration factor: 0';
     }
-
-    output.textContent=out.join("\n")
+    
+    outputArea.textContent=output;
     
   }
-
+  
 }
