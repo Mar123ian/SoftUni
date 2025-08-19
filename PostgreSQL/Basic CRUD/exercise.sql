a722bd2eb58f0b3039dbbd68e0e256b0cc9ac7d7
Mar123ian <mario.mg.sz.08@gmail.com>
2025-08-19 17:02:07 +0300

    Added exercises

---
+++ PostgreSQL/Basic CRUD/exercise.sql
@@ -0,0 +1,192 @@
+--EXERCISE 1
+SELECT * FROM cities
+ORDER BY id;
+
+--EXERCISE 2
+SELECT concat(name, ' ' ,state) AS cities_information,
+       area AS area_km2
+    FROM cities;
+
+--EXERCISE 3
+SELECT
+        DISTINCT name,
+        area AS area_km2
+    FROM cities
+ORDER BY name DESC;
+
+--EXERCISE 4
+SELECT
+    id,
+    concat(first_name, ' ', last_name) AS full_name,
+    job_title
+FROM employees
+ORDER BY first_name
+LIMIT 50;
+
+--EXERCISE 5
+SELECT
+    id AS id,
+    concat(first_name, ' ', middle_name, ' ', last_name) AS full_name,
+    hire_date
+FROM employees
+ORDER BY hire_date
+OFFSET 9;
+
+--EXERCISE 6
+SELECT
+    id,
+    concat(number, ' ', street) AS adress,
+    city_id
+    FROM addresses
+WHERE id >= 20;
+
+--EXERCISE 7
+SELECT
+    concat(number, ' ', street) AS adress,
+    city_id
+    FROM addresses
+WHERE city_id%2=0 AND city_id>=0
+ORDER BY city_id;
+
+--EXERCISE 8
+SELECT
+    name,
+    start_date,
+    end_date
+    FROM projects
+WHERE start_date>='2016-06-01 07:00:00' AND end_date<'2023-06-04 00:00:00'
+ORDER BY start_date;
+
+--EXERCISE 9
+SELECT
+    number,
+    street
+    FROM addresses
+WHERE id BETWEEN 50 AND 100 OR number < 1000;
+
+--EXERCISE 10
+SELECT
+    employee_id,
+    project_id
+    FROM employees_projects
+WHERE employee_id IN (200, 250) AND project_id NOT IN (50, 100);
+
+--EXERCISE 11
+SELECT
+    name,
+    start_date
+    FROM projects
+WHERE name IN ('Mountain', 'Road', 'Touring')
+LIMIT 20;
+
+--EXERCISE 12
+SELECT
+    concat(first_name,' ', last_name) AS full_name,
+    job_title,
+    salary
+FROM employees
+WHERE salary IN (12500, 14000, 23600, 25000)
+ORDER BY salary DESC;
+
+--EXERCISE 13
+SELECT
+    id,
+    first_name,
+    last_name
+FROM employees
+WHERE middle_name IS NULL
+LIMIT 3;
+
+--EXERCISE 14
+INSERT INTO departments(department, manager_id)
+VALUES ('Finance', 3),
+
+('Information Services', 42),
+
+('Document Control', 90),
+
+('Quality Assurance', 274),
+
+('Facilities and Maintenance', 218),
+
+('Shipping and Receiving', 85),
+
+('Executive', 109);
+
+--EXERCISE 15
+CREATE TABLE company_chart AS
+    SELECT
+        concat(first_name,' ', last_name) AS full_name,
+        job_title,
+        department_id,
+        manager_id
+        FROM employees;
+
+--EXERCISE 16
+UPDATE projects
+SET end_date=start_date+ INTERVAL '5 months'
+WHERE end_date IS NULL;
+
+--EXERCISE 17
+UPDATE employees
+SET salary=salary+1500,
+    job_title=concat('Senior ',job_title)
+WHERE hire_date BETWEEN 'January 1, 1998' AND 'January 5, 2000';
+
+--EXERCISE 18
+DELETE FROM addresses
+WHERE city_id IN (5, 17, 20, 30);
+
+--EXERCISE 19
+CREATE VIEW view_company_chart AS
+    SELECT
+        full_name,
+        job_title
+        FROM company_chart
+    WHERE manager_id=184;
+
+--EXERCISE 20
+CREATE VIEW view_addresses AS
+    SELECT
+        concat(e.first_name,' ', e.last_name) AS full_name,
+        e.department_id,
+        concat(a.number, ' ', a.street) AS address
+        FROM employees AS e, addresses AS a
+    WHERE e.address_id=a.id
+    ORDER BY address;
+
+--EXERCISE 21
+ALTER VIEW view_addresses
+RENAME TO view_employee_addresses_info;
+
+--EXERCISE 22
+DROP VIEW view_company_chart;
+
+--EXERCISE 23
+UPDATE projects
+SET name = upper(name);
+
+--EXERCISE 24
+CREATE VIEW view_initials AS
+    SELECT
+        LEFT(first_name,2) AS initial,
+        last_name
+        FROM employees
+    ORDER BY last_name;
+
+--EXERCISE 25
+SELECT
+    name,
+    start_date
+    FROM projects
+WHERE starts_with(name, 'MOUNT')
+ORDER BY id;
+
+
+
+
+
+
+
+
+
--- PostgreSQL/Data Types. Working with Tables/exercise.sql
+++ PostgreSQL/Data Types. Working with Tables/exercise.sql
@@ -1,16 +1,18 @@
---CODE FOR ALL 17 EXERCISES SEPARATED WITH FREE SPACE BELLOW
-
+--EXERCISE 0
 CREATE DATABASE minions_db;
 
+--EXERCISE 1
 CREATE TABLE minions (
 	id SERIAL PRIMARY KEY,
 	name VARCHAR(30),
 	age INT
 );
 
+--EXERCISE 2
 ALTER TABLE minions
 RENAME TO minions_info;
 
+--EXERCISE 3
 ALTER TABLE minions_info
 ADD COLUMN code CHAR(4);
 ALTER TABLE minions_info
@@ -18,18 +20,22 @@ ADD COLUMN task TEXT;
 ALTER TABLE minions_info
 ADD COLUMN salary DECIMAL(8,3);
 
+--EXERCISE 4
 ALTER TABLE minions_info
 RENAME COLUMN salary TO banana;
 
+--EXERCISE 5
 ALTER TABLE minions_info
 ADD COLUMN email VARCHAR(20);
 ALTER TABLE minions_info
 ADD COLUMN equipped BOOLEAN NOT NULL;
 
+--EXERCISE 6
 CREATE TYPE type_mood AS ENUM ('happy', 'relaxed', 'stressed', 'sad');
 ALTER TABLE minions_info
 ADD COLUMN mood type_mood;
 
+--EXERCISE 7
 ALTER TABLE minions_info
 ALTER COLUMN age SET DEFAULT 0;
 ALTER TABLE minions_info
@@ -37,21 +43,26 @@ ALTER COLUMN name SET DEFAULT '';
 ALTER TABLE minions_info
 ALTER COLUMN code SET DEFAULT '';
 
+--EXERCISE 8
 ALTER TABLE minions_info
 ADD CONSTRAINT unique_containt UNIQUE(email,id);
 ALTER TABLE minions_info
 ADD CONSTRAINT banana_check CHECK(banana>0);
 
+--EXERCISE 9
 ALTER TABLE minions_info
 ALTER COLUMN task TYPE VARCHAR(150);
 
+--EXERCISE 10
 ALTER TABLE minions_info
 ALTER COLUMN equipped 
 DROP NOT NULL;
 
+--EXERCISE 11
 ALTER TABLE minions_info
 DROP COLUMN age;
 
+--EXERCISE 12
 CREATE TABLE minions_birthdays (
 	id INT NOT NULL PRIMARY KEY,
 	name VARCHAR(50),
@@ -61,6 +72,7 @@ CREATE TABLE minions_birthdays (
 	party TIMESTAMPTZ
 );
 
+--EXERCISE 13
 INSERT INTO minions_info(name, code, task, banana, email, equipped, mood)
 VALUES 
 ('Mark', 'GKYA', 'Graphing Points', 3265.265, 'mark@minion.com', false, 'happy'),
@@ -71,10 +83,14 @@ VALUES
 ('Norbert', 'FEWB', 'Testing', 3265.500, 'norbert@minion.com', true, 'sad'),
 ('Donny', 'L', 'Make a Map', 8.452, 'donny@minion.com', true, 'happy');
 
+--EXERCISE 14
 SELECT name, task, email, banana FROM minions_info;
 
+--EXERCISE 15
 TRUNCATE minions_info;
 
+--EXERCISE 16
 DROP TABLE minions_birthdays;
 
+--EXERCISE 17
 DROP DATABASE minions_db;
